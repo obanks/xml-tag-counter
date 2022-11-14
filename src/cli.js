2e075156c32067065d979a853c8c41005e74742c
Oliver Banks <oliver.banks@bbc.co.uk>
2022-11-14 18:48:57 +0000

    Initial commit

---
+++ src/cli.js
@@ -0,0 +1,70 @@
+import chalk from "chalk";
+import commandLineArgs from "command-line-args";
+import glob from "glob";
+import expandTilde from "expand-tilde";
+import { load } from "cheerio";
+import fs from "fs";
+
+const optionDefinitions = [
+  { name: "articlesRoot", type: String, defaultOption: true },
+];
+const blockList = ['body', 'html', 'head'];
+
+const unescapeHTML = (escapedHTML) => {
+  return escapedHTML
+    .replace(/&lt;/g, "<")
+    .replace(/&gt;/g, ">")
+    .replace(/&amp;/g, "&");
+};
+
+export const cli = () => {
+  const articlesRoot = expandTilde(
+    commandLineArgs(optionDefinitions).articlesRoot
+  );
+  if (!articlesRoot) {
+    console.log("The articlesRoot parameter must be provided.");
+    process.exit(1);
+  }
+  console.log(chalk.magenta(`Parsing articles in ${articlesRoot}...`));
+  var sourceFiles = glob.sync(`${articlesRoot}/*.xml`);
+  console.log(sourceFiles);
+
+  const allKnownTags = [];
+  sourceFiles.forEach((file) => {
+    console.log(`Parsing ${file}`);
+    const fileContents = fs.readFileSync(file, "utf8");
+    let $ = load(fileContents);
+    const contentBody = $("content-body");
+    if (!contentBody.length) {
+      console.log("File is not an article, skipping...");
+      return;
+    }
+    $ = load(unescapeHTML(contentBody.html()));
+    const tags = $("*");
+    tags.each((_, tag) => {
+      const currentTagName = tag.name;
+      if (blockList.includes(currentTagName)) {
+        console.log('Ignoring tag');
+        return;
+      }
+      const existingTag = allKnownTags.find(
+        (tag) => tag.name === currentTagName
+      );
+      if (existingTag) {
+        console.log(
+          chalk.greenBright(
+            `Found instance of known tag ${currentTagName}, current count is ${
+              existingTag.count + 1
+            }`
+          )
+        );
+        existingTag.count += 1;
+      } else {
+        allKnownTags.push({ name: currentTagName, count: 1 });
+        console.log(chalk.redBright(`Found new tag ${currentTagName}.`));
+      }
+    });
+  });
+  console.log("scanning complete!");
+  console.log(allKnownTags);
+};
