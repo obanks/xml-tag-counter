86483259a0943a0a531e099252634ebabffc1ab3
Oliver Banks <oliver.banks@bbc.co.uk>
2022-12-13 17:50:25 +0000

    Rework script to count embed tags

--- src/cli.js
+++ src/cli.js
@@ -17,20 +17,14 @@ const unescapeHTML = (escapedHTML) => {
     .replace(/&amp;/g, "&");
 };
 
-export const findTags = (file) => {
-  console.log(`Parsing ${file}`);
-  const fileContents = fs.readFileSync(file, "utf8");
-  let $ = load(fileContents);
-  const contentBody = $("content-body");
-  if (!contentBody.length) {
-    console.log("File is not an article, skipping...");
-    return;
-  }
-  $ = load(unescapeHTML(contentBody.html()));
-  return $("*");
-};
+export const findTags = (file) => {};
 
-const getFileName = filePath => filePath.replace(/^.*[\\\/]/, '')
+const getFileName = (filePath) => filePath.replace(/^.*[\\\/]/, "");
+
+const regexRiddle = /^https?:\/\/www\.riddle.com/;
+const regexYoutube = /^https?:\/\/www\.youtube.com/;
+const regexTwitter = /^https?:\/\/(?:www\.)?twitter.com/;
+const regexInstagram = /^https?:\/\/www\.instagram.com/;
 
 export const cli = () => {
   const articlesRoot = expandTilde(
@@ -40,55 +34,49 @@ export const cli = () => {
     console.log("The articlesRoot parameter must be provided.");
     process.exit(1);
   }
-  console.log(chalk.magenta(`Parsing articles in ${articlesRoot}...`));
+  // console.log(chalk.magenta(`Parsing articles in ${articlesRoot}...`));
   var sourceFiles = glob.sync(`${articlesRoot}/*.xml`);
-  console.log(sourceFiles);
+  // console.log(sourceFiles);
+  const youtubeEmbedFiles = new Set();
+  const twitterEmbedFiles = new Set();
+  const instagramEmbedFiles = new Set();
+  const riddleEmbedFiles = new Set();
 
-  const allKnownTags = [];
   sourceFiles.forEach((file) => {
-    const tags = findTags(file);
-    tags?.each((_, tag) => {
-      const currentTagName = tag.name;
-      const currentTagAttributes = Object.keys(tag.attribs);
-      if (blockList.includes(currentTagName)) {
-        console.log("Tag is on the blocklist, ignoring it");
-        return;
-      }
-      const existingTag = allKnownTags.find(
-        (tag) => tag.name === currentTagName
-      );
-      if (existingTag) {
-        console.log(
-          chalk.greenBright(
-            `Found instance of known tag ${currentTagName}, current count is ${
-              existingTag.count + 1
-            }`
-          )
-        );
-        existingTag.count += 1;
-        currentTagAttributes?.forEach((attribute) => {
-          const matchingAttribute = existingTag.attributes.find(
-            (existingAttribute) => existingAttribute === attribute
-          );
-          if (!matchingAttribute) {
-            existingTag.attributes.push(attribute);
-          }
-        });
-        const knownFile = existingTag.examples.find((example) => example === file);
-        if (!knownFile && existingTag.examples.length < 10) {
-          existingTag.examples.push(getFileName(file));
-        }
-      } else {
-        console.log(chalk.redBright(`Found new tag ${currentTagName}.`));
-        allKnownTags.push({
-          name: currentTagName,
-          count: 1,
-          attributes: currentTagAttributes,
-          examples: [getFileName(file)],
-        });
+    // console.log(`Parsing ${file}`);
+    const fileContents = fs.readFileSync(file, "utf8");
+    let $ = load(unescapeHTML(fileContents));
+    const contentBody = $("content-body");
+    if (!contentBody.length) {
+      // console.log("File is not an article, skipping...");
+      return;
+    }
+    // console.log("scanning", file);
+    contentBody.children().map((_, element) => {
+      const elementText = $(element).text();
+      // console.log("testing node..", elementText);
+      const filename = file.toString().substring(file.toString().lastIndexOf('/') + 1)
+      if (regexYoutube.test(elementText)) {
+        youtubeEmbedFiles.add(elementText);
+      } else if (regexTwitter.test(elementText)) {
+        twitterEmbedFiles.add(elementText);
+      } else if (regexInstagram.test(elementText)) {
+        instagramEmbedFiles.add(elementText);
+      } else if (regexRiddle.test(elementText)) {
+        riddleEmbedFiles.add(elementText);
       }
     });
   });
   console.log(chalk.magenta(`Scanning complete!`));
-  console.log(allKnownTags);
+  console.log(
+    "Files containing Instagram embeds: ",
+    instagramEmbedFiles.size
+  );
+  console.log(instagramEmbedFiles);
+  // console.log("Files containing Youtube embeds: ", youtubeEmbedFiles.size);
+  // console.log(youtubeEmbedFiles);
+  // console.log("Files containing Twitter embeds: ", twitterEmbedFiles.size);
+  // console.log(twitterEmbedFiles);
+  // console.log("Files containing Riddle embeds: ", riddleEmbedFiles.size);
+  // console.log(riddleEmbedFiles);
 };
